6b0c5d97c22676d9df44a4143326312639905159
Pulkit-Rathore <pulkitrathore770@gmail.com>
2024-12-17 10:21:37 +0530

    defined endpoints for our CRUD operations using gofiber

--- internal/routes/movie/movie.go
+++ internal/routes/movie/movie.go
@@ -4,5 +4,15 @@ import "github.com/gofiber/fiber/v2"
 
 //The function SetupMovieRoutes takes a fiber.Router and handles endpoints to the movie model
 func SetupMovieRoutes(router fiber.Router) {
-
+	movie := router.Group("/movie")
+    // Create a Movie
+    note.Post("/", func(c *fiber.Ctx) error {})
+    // Read all Movie
+    note.Get("/", func(c *fiber.Ctx) error {})
+    // Read one Movie
+    note.Get("/:movieId", func(c *fiber.Ctx) error {})
+    // Update one Movie
+    note.Put("/:movieId", func(c *fiber.Ctx) error {})
+    // Delete one Movie
+    note.Delete("/:movieId", func(c *fiber.Ctx) error {})
 }
