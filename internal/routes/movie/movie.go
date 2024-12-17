16648ca352110ba8e949d3d90aa29a975285581f
Pulkit-Rathore <pulkitrathore770@gmail.com>
2024-12-17 10:17:06 +0530

    created movie routes file for endpoint calls with the help of gofiber

---
+++ internal/routes/movie/movie.go
@@ -0,0 +1,8 @@
+package movieRoutes
+
+import "github.com/gofiber/fiber/v2"
+
+//The function SetupMovieRoutes takes a fiber.Router and handles endpoints to the movie model
+func SetupMovieRoutes(router fiber.Router) {
+
+}
