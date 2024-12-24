657344fa7ff2162419129e95e53b088576359fbe
Pulkit-Rathore <pulkitrathore770@gmail.com>
2024-12-24 17:02:48 +0530

    commented gorm import and model to avoid meta data involvement

--- internal/model/model.go
+++ internal/model/model.go
@@ -2,11 +2,11 @@ package model
 
 import (
     "github.com/google/uuid"
-    "gorm.io/gorm"
+  //  "gorm.io/gorm"
 )
 
 type Movie struct {
-    gorm.Model           // Adds some metadata fields to the table
+    //gorm.Model           // Adds some metadata fields to the table
     ID         uuid.UUID `gorm:"type:uuid"` // Explicitly specify the type to be uuid
     Title      string
     Director   string
